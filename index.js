dcea0685489b2e54478fdc77edb97e8a2f454fdb
leahthompson01 <leahmthompson03@gmail.com>
2022-05-29 05:18:36 -0400

    made it to where I'm only receiving data from the api on the first click

--- index.js
+++ index.js
@@ -6,32 +6,42 @@ const villagerSpecies = document.querySelector('#species')
 const villagerPersonality = document.querySelector('#personality')
 const villagerSaying = document.querySelector('#saying')
 const villagerBirthday = document.querySelector('#birthday')
+//count to keep track of how many times button has been clicked
+let count = 0;
+let dataObj
+if(count === 0){
+    btn.addEventListener('click', getCharacterFirst)
+}else {
+    btn.addEventListener('click', changeCardInfo)
+}
 
-btn.addEventListener('click', getCharacter)
-function getCharacter(){
+function getCharacterFirst(){
+        fetch(url)
+        .then(res => res.json())
+        .then( data => {
+                btn.classList.remove('fixed')
+                dataObj = data
+                changeCardInfo()
+        })
+    }   
+function changeCardInfo(){
     let random = Math.floor(Math.random() * 391)
-fetch(url)
-.then(res => res.json())
-.then(data => {
-    btn.classList.remove('fixed')
     document.querySelector('#card').classList.remove('hidden')
-    if (document.querySelector('span')){
-        document.querySelector('.spec').remove()
-        document.querySelector('.pers').remove()
-    } 
-    villagerImg.src = data[random].image_uri
-    villagerName.innerText = data[random].name['name-USen']
-    let span1 = document.createElement('span')
-    span1.classList.add('spec')
-    villagerSpecies.appendChild(span1)
-    span1.innerHTML = data[random].species 
-    let span2 = document.createElement('span')
-    span2.classList.add('pers')
-    villagerPersonality.appendChild(span2)
-    span2.innerHTML = data[random].personality 
-    villagerSaying.innerText =  data[random].saying
-    villagerBirthday.innerText =  data[random]['birthday-string']
-
-
-})
+        if (document.querySelector('span')){
+            document.querySelector('.spec').remove()
+            document.querySelector('.pers').remove()
+        } 
+        villagerImg.src = dataObj[random].image_uri
+        villagerName.innerText = dataObj[random].name['name-USen']
+        let span1 = document.createElement('span')
+        span1.classList.add('spec')
+        villagerSpecies.appendChild(span1)
+        span1.innerHTML = dataObj[random].species 
+        let span2 = document.createElement('span')
+        span2.classList.add('pers')
+        villagerPersonality.appendChild(span2)
+        span2.innerHTML = dataObj[random].personality 
+        villagerSaying.innerText =  dataObj[random].saying
+        villagerBirthday.innerText =  dataObj[random]['birthday-string']
+        count++    
 }
